2d54ec8ef051933d8fd4d5a1ecc78904eb465912
Vivian-Xiao1025 <67659378+Vivian-Xiao1025@users.noreply.github.com>
2022-11-01 20:28:31 -0500

    water pump now working

--- Electric Components/Temperature_sensor/Temperature_sensor.ino
+++ Electric Components/Temperature_sensor/Temperature_sensor.ino
@@ -1,32 +1,49 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 5
+#define ONE_WIRE_BUS 11 
 
-// code: https://create.arduino.cc/projecthub/iotboys/how-to-use-ds18b20-water-proof-temperature-sensor-2adecc
-// schematics: https://arduinogetstarted.com/tutorials/arduino-temperature-sensor
+//declaring the constants for the sensor 
+const int ENA_A_PIN = 7; 
+const int IN1_PIN = 5; 
+const int IN2_PIN = 6; 
 
-OneWire oneWire(ONE_WIRE_BUS);
+// code: https://create.arduino.cc/projecthub/iotboys/how-to-use-ds18b20-water-proof-temperature-sensor-2adecc 
+// schematics: https://arduinogetstarted.com/tutorials/arduino-temperature-sensor 
 
-DallasTemperature sensors(&oneWire);
+OneWire oneWire(ONE_WIRE_BUS); 
 
- float Celcius=0;
- float Fahrenheit=0;
-void setup(void)
-{
-  
-  Serial.begin(9600);
-  sensors.begin();
-}
+DallasTemperature sensors(&oneWire); 
 
-void loop(void)
-{ 
+float Celcius=0; 
+float Fahrenheit=0; 
+
+void setup(void) { 
+  Serial.begin(9600); 
+
+  //starts the sensor 
+  sensors.begin(); 
+
+  //declaring the state of the pins for the pump 
+  pinMode(ENA_A_PIN, OUTPUT); 
+  pinMode(IN1_PIN, OUTPUT); 
+  pinMode(IN2_PIN, OUTPUT); 
+} 
+
+void loop(void) { 
+
+  //getting the temperature values 
   sensors.requestTemperatures(); 
-  Celcius=sensors.getTempCByIndex(0);
-  Fahrenheit=sensors.toFahrenheit(Celcius);
-  Serial.print(" C  ");
-  Serial.print(Celcius);
-  Serial.print(" F  ");
-  Serial.println(Fahrenheit);
-  delay(1000);
+  Celcius=sensors.getTempCByIndex(0); 
+  Fahrenheit=sensors.toFahrenheit(Celcius); 
+  Serial.print(" C "); 
+  Serial.print(Celcius); 
+  Serial.print(" F "); 
+  Serial.println(Fahrenheit); 
+  delay(1000); 
+
+  // driving the pump 
+  digitalWrite(IN1_PIN, LOW); 
+  digitalWrite(IN2_PIN, HIGH); 
+  digitalWrite(ENA_A_PIN, 250); 
 }
